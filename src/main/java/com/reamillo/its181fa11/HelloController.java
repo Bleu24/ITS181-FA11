e1351c6446716558fe9f9b20d9fa83214813913f
Bryan Reamillo <bryanreamillo2403@gmail.com>
2024-06-01 16:19:20 +0800

    added via git

---
+++ src/main/java/com/reamillo/its181fa11/HelloController.java
@@ -0,0 +1,14 @@
+package com.reamillo.its181fa11;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.reamillo.its181fa11 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.reamillo.its181fa11 to javafx.fxml;
+    exports com.reamillo.its181fa11;
+}
